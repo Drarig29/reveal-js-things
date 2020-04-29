5ac73d9f62e5666b51aea674cccdc543aac94670
Corentin Girard <corentingirard.dev@gmail.com>
2020-04-29 11:55:59 +0200

    Create inject-theme.js

---
+++ inject-theme.js
@@ -0,0 +1,18 @@
+const themes = ['black', 'beige', 'blood', 'league', 'moon', 'night', 'serif', 'simple', 'sky', 'solarized', 'white'];
+const themeStr = themes.map((elem, index) => `${elem} (${index})`).join(', ');
+
+function setTheme(index) {
+  const theme = themes[index];
+  const src = document.getElementById("theme").href.replace(/(css\/theme)\/\w+\.css/, `$1/${theme}.css`);
+  document.getElementById("theme").href = src;
+}
+
+function changeTheme() {
+  const number = prompt(`Entrez le numéro d'un thème [${themeStr}]`, 0).trim();
+  if (number.match(/^\d+$/))
+    setTheme(number);
+  else
+    alert("Ce n'est pas un numéro valide !");
+}
+
+changeTheme();
