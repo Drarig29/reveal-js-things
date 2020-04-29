34627ba232e7e591c4355105479608215ba043f9
Corentin Girard <corentingirard.dev@gmail.com>
2020-04-29 12:15:49 +0200

    Update inject-theme.js

--- inject-theme.js
+++ inject-theme.js
@@ -1,7 +1,7 @@
 // https://github.com/hakimel/reveal.js/blob/master/css/theme/README.md
 
-const themes = ['black', 'beige', 'blood', 'league', 'moon', 'night', 'serif', 'simple', 'sky', 'solarized', 'white'];
-const themeStr = themes.map((elem, index) => `${elem} (${index})`).join(', ');
+var themes = ['black', 'beige', 'blood', 'league', 'moon', 'night', 'serif', 'simple', 'sky', 'solarized', 'white'];
+var themeStr = themes.map((elem, index) => `${elem} (${index})`).join(', ');
 
 function setTheme(index) {
   const theme = themes[index];
