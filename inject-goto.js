e7d3b0065d51bc7297a7157d49fda25c173a7ae7
Corentin Girard <corentingirard.dev@gmail.com>
2020-04-29 11:33:33 +0200

    Create inject-goto.js

---
+++ inject-goto.js
@@ -0,0 +1,16 @@
+Reveal.goto = slideNumber => {
+    let indices = Reveal.getIndices(Reveal.getSlides()[slideNumber - 1]);
+    Reveal.slide(indices.h, indices.v);
+};
+
+Reveal.getCurrentSlideNumber = () => Reveal.getSlides().indexOf(Reveal.getCurrentSlide()) + 1;
+
+function gotoSlide() {
+    let number = prompt("Entrez un numéro de diapo :", Reveal.getCurrentSlideNumber()).trim();
+    if (number.match(/^\d+$/))
+        Reveal.goto(number);
+    else
+        alert("Ce n'est pas un numéro valide !");
+}
+
+gotoSlide();
